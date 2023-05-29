3ea3a7ac7b117bc9e5f7fd42d7a8dc1bf3a1cfbb
EJ <hanej93@naver.com>
2023-05-29 21:57:00 +0900

    로그아웃 및 인증에 따른 화면 보안 처리

--- src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,7 +1,14 @@
 package io.security.corespringsecurity.controller.login;
 
+import org.apache.catalina.security.SecurityUtil;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -9,6 +16,17 @@ public class LoginController {
 	@GetMapping("/login")
 	public String login() {
 		return "user/login/login";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication != null) {
+			new SecurityContextLogoutHandler().logout(request, response, authentication);
+		}
+
+		return "redirect:/login";
 	}
 
 }
--- src/main/java/io/security/corespringsecurity/controller/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/controller/security/configs/SecurityConfig.java
@@ -3,7 +3,6 @@ package io.security.corespringsecurity.controller.security.configs;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
