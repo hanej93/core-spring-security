2c48136bdb5308ecc0e1e91ed6e0b65ae343e495
EJ <hanej93@naver.com>
2023-05-29 17:52:47 +0900

    정적 자원 관리 - WebIgnore 설정

--- src/main/java/io/security/corespringsecurity/controller/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/controller/security/configs/SecurityConfig.java
@@ -1,10 +1,12 @@
 package io.security.corespringsecurity.controller.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,7 +35,7 @@ public class SecurityConfig {
 		UserDetails manager = User.builder()
 			.username("manager")
 			.password(password)
-			.roles("MANAGER")
+			.roles("USER", "MANAGER")
 			.build();
 
 		UserDetails admin = User.builder()
@@ -46,6 +48,12 @@ public class SecurityConfig {
 	}
 
 	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring()
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
+
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
@@ -55,7 +63,7 @@ public class SecurityConfig {
 		http
 			.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
 				authorizationManagerRequestMatcherRegistry
-					.requestMatchers("/").permitAll()
+					.requestMatchers("/", "/user").permitAll()
 					.requestMatchers("/mypage").hasRole("USER")
 					.requestMatchers("/messages").hasRole("MANAGER")
 					.requestMatchers("/config").hasRole("ADMIN")
--- src/main/java/io/security/corespringsecurity/controller/user/MessageController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/MessageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MessageController {
 
-	@GetMapping("message")
+	@GetMapping("/messages")
 	public String messages() {
 		return "user/messages";
 	}
