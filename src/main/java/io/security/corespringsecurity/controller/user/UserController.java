5894cc2ce712b1cc672c7a1742b5332846dfec50
EJ <hanej93@naver.com>
2023-05-29 17:26:37 +0900

    core project init

---
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -0,0 +1,19 @@
+package io.security.corespringsecurity.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/mypage")
+	public String myPage() throws Exception {
+		return "user/mypage";
+	}
+
+	@GetMapping("/users")
+	public String createUser() {
+		return "user/login/register";
+	}
+
+}
